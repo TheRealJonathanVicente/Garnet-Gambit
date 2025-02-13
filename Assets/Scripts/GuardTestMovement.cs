48022675c79da180080229d0fef67d7d4367a04f
Krampuso <al849783@ucf.edu>
2025-02-12 20:20:05 -0500

    upload

--- Assets/Scripts/GuardTestMovement.cs
+++ Assets/Scripts/GuardTestMovement.cs
@@ -7,16 +7,32 @@ public class GuardTestMovement : MonoBehaviour
 {
     public NavMeshAgent agent;
     public Transform testLoc;
+    public Transform Loc1;
+    public Transform Loc2;
+
+    public bool firstMove = true;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(firstMove == true){
+            agent.SetDestination(Loc1.position);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //agent.SetDestination(testLoc.position);
-        agent.Move(Vector3.forward);
+       // NavMeshHit hit;
+        
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        firstMove = false;
+        agent.SetDestination(Loc2.position);
+    }
+
+    
 }
